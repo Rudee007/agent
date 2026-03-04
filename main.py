d93189ee5a3713164d06fa0743838a48a2437f54
Rudee007 <nikuswain01171432@gmail.com>
2026-03-04 13:40:28 +0530

    added verbose

--- main.py
+++ main.py
@@ -16,16 +16,25 @@ client = genai.Client(api_key=api_key)
 
 def main():
 
-#   parser help us to take the input from the commmand line it self
+#   parser help us to take the input from the commmand line it self 
     parser = argparse.ArgumentParser(description='ChatBot')
     parser.add_argument("user_prompt", type=str, help='user prompt')
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
+
+
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+
 
     response = client.models.generate_content(
     model="gemini-2.5-flash", contents=messages
     )
+
+    if args.verbose:
+        print(f"user input: {args.user_input}")
+        print(f"prompt tokens: {response}")
 
     print(response.text)
     
